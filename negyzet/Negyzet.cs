17abfa66466b46ef179b913cd0c91ce9dfa18da6
Unknown <xnorby95x@gmail.com>
2018-02-20 14:44:00 +0100

    first version is finish

---
+++ negyzet/Negyzet.cs
@@ -0,0 +1,20 @@
+﻿using negyszogek;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negyzet
+{
+    public class Negyzet: Teglalap
+    {
+
+
+        public Negyzet(int v):base(v,v)
+        {
+            
+        }
+    }
+
+}
---
+++ negyzet/teglalap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negyszogek    
+{
+    public class Teglalap: Negyszog
+    {
+      public Teglalap(int a, int b) : base(a, b, a, b) { }
+    }
+}
